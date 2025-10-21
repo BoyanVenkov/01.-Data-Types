3b5ad2c9578900494e76076aa8228df5b28a2cf4
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-21 19:21:02 +0300

    Add project files.

---
+++ 01. Data Types/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string type = Console.ReadLine();
+        string input = Console.ReadLine();
+
+        PrintResult(type, input);
+    }
+
+    static void PrintResult(string type, string input)
+    {
+        if (type == "int")
+        {
+            int number = int.Parse(input);
+            Console.WriteLine(number * 2);
+        }
+        else if (type == "real")
+        {
+            double number = double.Parse(input);
+            Console.WriteLine($"{number * 1.5:F2}");
+        }
+        else if (type == "string")
+        {
+            Console.WriteLine($"${input}$");
+        }
+    }
+}
