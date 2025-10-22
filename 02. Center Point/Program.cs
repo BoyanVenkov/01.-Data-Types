090a45dc41958b8a65701acf391f3ba03d6783e4
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-22 06:52:27 +0300

    Added a New Project

---
+++ 02. Center Point/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double x1 = double.Parse(Console.ReadLine());
+        double y1 = double.Parse(Console.ReadLine());
+        double x2 = double.Parse(Console.ReadLine());
+        double y2 = double.Parse(Console.ReadLine());
+
+        double distance1 = x1 * x1 + y1 * y1;
+        double distance2 = x2 * x2 + y2 * y2;
+
+        if (distance1 <= distance2)
+        {
+            Console.WriteLine($"({x1}, {y1})");
+        }
+        else
+        {
+            Console.WriteLine($"({x2}, {y2})");
+        }
+    }
+}
