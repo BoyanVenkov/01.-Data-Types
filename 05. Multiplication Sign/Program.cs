58701d6d01367090f517b5c8a657dc240136e479
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-26 07:00:53 +0200

    Added a New Project

---
+++ 05. Multiplication Sign/Program.cs
@@ -0,0 +1,43 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+MultiplicationSign(num1, num2, num3);
+
+static void MultiplicationSign(int a, int b, int c)
+{
+    int negativeCount = 0;
+    if (a == 0 || b == 0 || c == 0)
+    {
+        Console.WriteLine("zero");
+        return;
+    }
+    if (a < 0)
+    {
+        negativeCount++;
+    }
+    if (b < 0)
+    {
+        negativeCount++;
+    }
+    if (c < 0)
+    {
+        negativeCount++;
+    }
+    if (negativeCount % 2 != 0)
+    {
+        Console.WriteLine("negative");
+    }
+    if (negativeCount % 2 == 0)
+    {
+        Console.WriteLine("positive");
+        return;
+    }
+    if (a > 0 && b > 0 && c > 0)
+    {
+        Console.WriteLine("positive");
+    }
+}
+
+
+
